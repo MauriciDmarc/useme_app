d5940d06152d11a881e487f7b727a5da366d01c2
Maurici Dmarco <mauricidmarco@gmail.com>
2023-10-13 22:45:33 -0300

    -finalizado: SingnIn Screen

--- lib/auth/signin_screen.dart
+++ lib/auth/signin_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:useme_app/components/custom_textformfield.dart';
 
@@ -10,20 +11,122 @@ class SignInScreen extends StatelessWidget {
       backgroundColor: const Color(0xFF1A1A1A),
       body: Column(
         children: [
-          Expanded(child: Container()),
+          //logo
+          Expanded(
+              child: Padding(
+            padding: const EdgeInsets.all(32),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Image.asset(
+                  'image/logo.png',
+                  fit: BoxFit.scaleDown,
+                ),
+                const Divider(
+                  color: Colors.white,
+                  thickness: 1,
+                  indent: 32,
+                  endIndent: 32,
+                ),
+                SizedBox(
+                  height: 50,
+                  child: DefaultTextStyle(
+                    style: const TextStyle(
+                        fontSize: 24,
+                        color: Colors.white,
+                        fontStyle: FontStyle.italic),
+                    child: Center(
+                      child: AnimatedTextKit(
+                          repeatForever: true,
+                          pause: Duration.zero,
+                          animatedTexts: [
+                            FadeAnimatedText("Suplementos"),
+                            FadeAnimatedText("Roupas"),
+                            FadeAnimatedText("Produtos Naturais"),
+                          ]),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )),
+          //formulario
           Container(
             padding: const EdgeInsets.all(32),
             decoration: const BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.vertical(top: Radius.circular(50))),
-            child: const Column(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 //email
-                CustomTextFormField(),
+                const CustomTextFormField(
+                  iconData: Icons.email,
+                  label: "Email",
+                ),
                 //senha
-                CustomTextFormField(),
+                const CustomTextFormField(
+                  iconData: Icons.password,
+                  label: "Senha",
+                  visibility: true,
+                ),
+                //botao entrar
+                Padding(
+                  padding: const EdgeInsets.only(top: 8),
+                  child: SizedBox(
+                      height: 50,
+                      child: ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(16))),
+                          child: const Text(
+                            "ENTRAR",
+                            style: TextStyle(fontSize: 18),
+                          ))),
+                ),
                 //esqueci a senha
-                //botao
+                TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      "Esqueci a senha",
+                      style: TextStyle(color: Colors.blue),
+                    )),
+                //divisor
+                const Padding(
+                  padding: EdgeInsets.only(bottom: 16),
+                  child: Row(children: [
+                    Expanded(
+                        child: Divider(
+                      thickness: 2,
+                      endIndent: 8,
+                    )),
+                    Text(
+                      "Ou",
+                      style: TextStyle(color: Colors.black54),
+                    ),
+                    Expanded(
+                        child: Divider(
+                      thickness: 2,
+                      indent: 8,
+                    ))
+                  ]),
+                ),
+                //botao criar
+                SizedBox(
+                    height: 50,
+                    child: OutlinedButton(
+                        onPressed: () {},
+                        style: OutlinedButton.styleFrom(
+                            side: const BorderSide(
+                                width: 2, color: Colors.deepPurple),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(16))),
+                        child: const Text(
+                          "CRIAR CONTA",
+                          style: TextStyle(fontSize: 16),
+                        ))),
               ],
             ),
           )
--- lib/components/custom_textformfield.dart
+++ lib/components/custom_textformfield.dart
@@ -1,19 +1,51 @@
 import 'package:flutter/material.dart';
 
-class CustomTextFormField extends StatelessWidget {
-  const CustomTextFormField({super.key});
+class CustomTextFormField extends StatefulWidget {
+  final IconData iconData;
+  final String label;
+  final bool visibility;
+  const CustomTextFormField(
+      {super.key,
+      required this.iconData,
+      required this.label,
+      this.visibility = false});
+
+  @override
+  State<CustomTextFormField> createState() => _CustomTextFormFieldState();
+}
+
+class _CustomTextFormFieldState extends State<CustomTextFormField> {
+  bool isobscure = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    isobscure = widget.visibility;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8),
       child: TextFormField(
+        obscureText: isobscure,
         decoration: InputDecoration(
           isDense: true,
-          label: Text("Email"),
-          prefixIcon: Icon(Icons.email),
+          label: Text(widget.label),
+          prefixIcon: Icon(widget.iconData),
+          suffixIcon: widget.visibility
+              ? IconButton(
+                  onPressed: () {
+                    setState(() {
+                      isobscure = !isobscure;
+                    });
+                  },
+                  icon:
+                      Icon(isobscure ? Icons.visibility : Icons.visibility_off))
+              : null,
           enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(6),
+              borderRadius: BorderRadius.circular(16),
               borderSide: const BorderSide(color: Colors.black12)),
           focusedBorder: OutlineInputBorder(
               borderRadius: BorderRadius.circular(28),
