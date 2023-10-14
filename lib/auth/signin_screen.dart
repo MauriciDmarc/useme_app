defce6315e4a8657f3c14816b8c82cb1ecda279c
Maurici Dmarco <mauricidmarco@gmail.com>
2023-10-13 21:09:03 -0300

    -modificado: cor do background principal

--- lib/auth/signin_screen.dart
+++ lib/auth/signin_screen.dart
@@ -7,7 +7,7 @@ class SignInScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black87,
+      backgroundColor: const Color(0xFF1A1A1A),
       body: Column(
         children: [
           Expanded(child: Container()),
