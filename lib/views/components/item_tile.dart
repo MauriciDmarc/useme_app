18aca9ce53ce14c324ee5b3d0063ff7abb2e0392
Maurici Dmarco <mauricidmarco@gmail.com>
2023-10-20 16:55:00 -0300

    -adicionado: tela de detalhes do produto

--- lib/views/components/item_tile.dart
+++ lib/views/components/item_tile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:useme_app/Services/util_services.dart';
 import 'package:useme_app/models/item_model.dart';
+import 'package:useme_app/views/home/product_screen.dart';
 
 class ItemTile extends StatelessWidget {
   ItemTile({super.key, required this.itemModel});
@@ -10,26 +11,54 @@ class ItemTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: Colors.white,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            //Imagem
-            Image.network(itemModel.imgUrl),
-            //Nome
-            Text(itemModel.itemName),
-            //Preço - Unidade
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(utilServices.priceToCurrency(itemModel.price)),
-                Text(" - ${itemModel.unit}")
-              ],
-            )
-          ],
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(MaterialPageRoute(builder: (c) {
+          return ProductScreen(
+            item: itemModel,
+            initialPrice: itemModel.price,
+          );
+        }));
+      },
+      child: Card(
+        color: Colors.white.withAlpha(230),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //Imagem
+              Hero(
+                  tag: itemModel.imgUrl,
+                  child: Image.network(itemModel.imgUrl)),
+              const Divider(
+                thickness: 1,
+                indent: 10,
+                endIndent: 10,
+              ),
+              //Nome
+              Text(
+                itemModel.itemName,
+                style:
+                    const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              //Preço - Unidade
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    utilServices.priceToCurrency(itemModel.price),
+                    style: const TextStyle(
+                        fontSize: 18,
+                        color: Colors.deepPurpleAccent,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Text(" - ${itemModel.unit}")
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/views/home/product_screen.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:useme_app/Services/util_services.dart';
+import 'package:useme_app/config/custom_colors.dart';
+import 'package:useme_app/models/item_model.dart';
+import 'package:useme_app/views/components/custom_quantityitem.dart';
+
+class ProductScreen extends StatefulWidget {
+  ProductScreen({super.key, required this.item, required this.initialPrice});
+  final ItemModel item;
+  int itemQuantity = 1;
+  final double initialPrice;
+
+  @override
+  State<ProductScreen> createState() => _ProductScreenState();
+}
+
+class _ProductScreenState extends State<ProductScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white.withAlpha(230),
+      body: Column(
+        children: [
+          Expanded(
+              child: Hero(
+                  tag: widget.item.imgUrl,
+                  child: Image.network(widget.item.imgUrl))),
+          Expanded(
+              child: Container(
+            padding: const EdgeInsets.all(24),
+            decoration: BoxDecoration(
+                color: CustomColors.customContrastColor,
+                borderRadius:
+                    const BorderRadius.vertical(top: Radius.circular(40)),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.grey.shade600, offset: const Offset(0, 5))
+                ]),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                //nome - qntd
+                Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        widget.item.itemName,
+                        maxLines: 2,
+                        style: const TextStyle(
+                            fontSize: 27,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                    ),
+                    CustomQuantityItem(
+                        value: widget.itemQuantity,
+                        result: (int quantity) {
+                          setState(() {
+                            widget.item.price = widget.initialPrice * quantity;
+                            widget.itemQuantity = quantity;
+                          });
+                        })
+                  ],
+                ),
+                //preço
+                Text(
+                  UtilServices().priceToCurrency(widget.item.price),
+                  style: const TextStyle(
+                      color: Colors.deepPurpleAccent,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold),
+                ),
+                //descrição
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: SingleChildScrollView(
+                      child: Text(widget.item.description * 4,
+                          style: const TextStyle(
+                            height: 1.5,
+                            color: Colors.white,
+                            fontSize: 18,
+                          )),
+                    ),
+                  ),
+                ),
+                //botão
+                SizedBox(
+                  height: 55,
+                  child: ElevatedButton.icon(
+                      style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15))),
+                      onPressed: () {},
+                      icon: const Icon(Icons.shopping_cart),
+                      label: const Text(
+                        "Adicionar ao Carrinho",
+                        textAlign: TextAlign.justify,
+                        style: TextStyle(fontSize: 20),
+                      )),
+                )
+              ],
+            ),
+          ))
+        ],
+      ),
+    );
+  }
+}
